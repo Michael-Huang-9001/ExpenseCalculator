590519bf4414119994b422bc59aead03147b05ee
Michael-Huang-9001 <Michael.Huang.9001@gmail.com>
2018-11-19 01:30:36 -0800

    added mlab

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost/expense-calc'
+    // url: 'mongodb://localhost/expense-calc'
+    url: 'mongodb://SolaceFZ:breaker15@ds211724.mlab.com:11724/expense-calc'
 };
