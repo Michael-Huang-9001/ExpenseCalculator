9425bb95cd2b65f897bcdff6356f967cbc929e2b
Michael-Huang-9001 <Michael.Huang.9001@gmail.com>
2018-11-29 03:00:38 -0800

    seperated mlabs setup

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: 'mongodb://localhost/expense-calc'
-    url: 'mongodb://SolaceFZ:breaker15@ds211724.mlab.com:11724/expense-calc'
+    url: 'mongodb://localhost/expense-calc'
 };
