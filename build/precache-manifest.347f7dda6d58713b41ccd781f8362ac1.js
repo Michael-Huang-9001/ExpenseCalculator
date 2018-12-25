84523f74b1246a7ff6823c402b3ae24605c142bc
Michael-Huang-9001 <Michael.Huang.9001@gmail.com>
2018-12-25 14:31:43 -0800

    Added build folder for production release

---
+++ build/precache-manifest.347f7dda6d58713b41ccd781f8362ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69a8f81756f815b177da",
+    "url": "/static/js/main.69a8f817.chunk.js"
+  },
+  {
+    "revision": "a92365c053c5b5a82250",
+    "url": "/static/js/1.a92365c0.chunk.js"
+  },
+  {
+    "revision": "69a8f81756f815b177da",
+    "url": "/static/css/main.bbe1b421.chunk.css"
+  },
+  {
+    "revision": "a92365c053c5b5a82250",
+    "url": "/static/css/1.6ace003c.chunk.css"
+  },
+  {
+    "revision": "4415097582b33bcac8358063bf476fe9",
+    "url": "/index.html"
+  }
+];
